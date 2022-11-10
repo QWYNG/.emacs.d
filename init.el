5c7af322493b741b1376f130564acefe36675521
hiroaki.osawa <hiroaki.osawa@smarthr.co.jp>
2022-11-10 20:37:43 +0900

    obisianの関数を有効化

--- init.el
+++ init.el
@@ -266,6 +266,8 @@
   :emacs>= 27.2
   :ensure t
   :after markdown-mode elgrep yaml)
+(obsidian-specify-path "~/Documents/Obsidian Vault/")
+(global-obsidian-mode t)
 
 (define-key global-map (kbd "C-t") 'other-window)
 (custom-set-variables
@@ -277,8 +279,7 @@
  '(menu-bar-mode nil)
  '(package-selected-packages (quote (blackout el-get hydra leaf-keywords leaf))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; custom-set-faces was added by Custom. ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
